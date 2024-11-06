e492916219148c8152bc9121349b781888999b7e
Automatax <mata.sanchez.999@gmail.com>
2024-11-06 06:34:25 -0300

    update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from collections import Counter
 import seaborn as sns
 import re
